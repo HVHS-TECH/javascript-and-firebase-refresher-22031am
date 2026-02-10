319cf47a844a3150199d8b9ff0361e6f42df0714
22031am <22031am@hvhs.school.nz>
2026-02-10 11:03:31 +0000

    SAVE! still working on it

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ messageSpace.innerHTML = "You've connected to the JavaScript!";
  Main Code
  *************************/
 
- /**********************
-Functions
+/**********************
+ Functions
 **********************/
 
+// Function for the "Press me!" button
 function pressBtn() {
-  
+    var heading = document.getElementById("hChange");
+    heading.innerHTML = "You pressed the button!";
+}
